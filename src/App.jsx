1c5dcc32d6d497063b94b42eb39875693c02e4bb
sergi256 <sergi256@users.noreply.github.com>
2025-11-02 10:38:35 +0100

    Add license information to App.jsx
    Added licensing information and comments to the top of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+/**
+ * Agenda Vacunal Catalunya
+ * Copyright (C) 2025 Sergi Catà de la Torra
+ * 
+ * Aquest programa és programari lliure: podeu redistribuir-lo i/o modificar-lo
+ * sota els termes de la Llicència Pública General Affero de GNU publicada per
+ * la Free Software Foundation, ja sigui la versió 3 de la Llicència o
+ * (a la vostra elecció) qualsevol versió posterior.
+ * 
+ * Aquest programa es distribueix amb l'esperança que sigui útil,
+ * però SENSE CAP GARANTIA; fins i tot sense la garantia implícita de
+ * COMERCIALITZACIÓ o IDONEÏTAT PER A UN PROPÒSIT PARTICULAR. Consulteu la
+ * Llicència Pública General Affero de GNU per a més detalls.
+ * 
+ * Hauríeu d'haver rebut una còpia de la Llicència Pública General Affero de GNU
+ * juntament amb aquest programa. Si no és així, consulteu <https://www.gnu.org/licenses/>.
+ */
+
 import { useState, useEffect } from 'react';
 import { X, Info, Calendar, Shield, AlertCircle, ExternalLink } from 'lucide-react';
 
